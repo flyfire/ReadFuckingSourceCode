2225490aadb29f3b1f68e03470f36f73ba7bddf1
Solarex <rh.hou.work@gmail.com>
2021-06-15 08:51:09 +0800

    rx thread

--- src/main/kotlin/EmptyObservable.kt
+++ src/main/kotlin/EmptyObservable.kt
@@ -1,7 +1,10 @@
 import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.schedulers.Schedulers
 
 fun main() {
-    var a = 0
+    var a: Int
     Observable.empty<Unit>()
         .doOnComplete {
             a = 1
@@ -15,4 +18,23 @@ fun main() {
         }.subscribe()
     a = 3
     println(a)
+
+    Observable.create(object : ObservableOnSubscribe<String> {
+        override fun subscribe(emitter: ObservableEmitter<String>) {
+            emitter.onNext("hello")
+        }
+    })
+        .subscribeOn(Schedulers.io())
+        .subscribeOn(Schedulers.computation())
+        .observeOn(Schedulers.newThread())
+        .observeOn(Schedulers.trampoline())
+        .subscribe(
+        {
+            println(it)
+        },
+        { throwable ->
+            throwable.printStackTrace()
+        }
+    )
+
 }
